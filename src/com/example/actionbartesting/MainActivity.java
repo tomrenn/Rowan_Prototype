008852d5f8ae865fb03585f6d4d0171ebed543bb
Thomas Renn <rennt79@students.rowan.edu>
2013-01-28 11:27:11 -0500

    Initial commit
    Empty android project utlizing ActionbarSherlock

---
+++ src/com/example/actionbartesting/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.actionbartesting;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
