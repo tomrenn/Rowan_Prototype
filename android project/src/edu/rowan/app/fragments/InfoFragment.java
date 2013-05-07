9e07be851e5eb5d5653b41cbfa9acae31bfbd773
Tom Renn <tomrenn@gmail.com>
2013-05-06 23:32:39 -0400

    Open facebook page in native facebook application
    if not possible, open in external web browser. Open in external browser
    in hopes it has users facebook information saved

--- android project/src/edu/rowan/app/fragments/InfoFragment.java
+++ android project/src/edu/rowan/app/fragments/InfoFragment.java
@@ -17,6 +17,9 @@
 package edu.rowan.app.fragments;
 
 import rowan.application.quickaccess.R;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -53,19 +56,28 @@ public class InfoFragment extends SimpleUpFragment implements OnClickListener{
 
 	@Override
 	public void onClick(View view) {
-		String url = "";
 
 		// setup link
 		switch (view.getId()) {
 		case R.id.githubButton:
-			url = GITHUB_ADDR;
+			openBrowserTo(GITHUB_ADDR);
 			break;
 		case R.id.facebookButton:
-			url = FACEBOOK_ADDR;
+			// http://stackoverflow.com/a/10636883/121654 
+			try {
+			      //try to open page in facebook native app.
+			      String uri = "fb://page/127595857272496";    //Cutsom URL
+			      Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+			      startActivity(intent);   
+			}catch (ActivityNotFoundException ex){
+			      // facebook native app isn't available, use browser.
+			      String uri = FACEBOOK_ADDR; //Normal URL  
+			      Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));    
+			      startActivity(i); 
+			}
 			break;
 		}
 		
-		openBrowserTo(url);
 	}
 	
 	@Override
